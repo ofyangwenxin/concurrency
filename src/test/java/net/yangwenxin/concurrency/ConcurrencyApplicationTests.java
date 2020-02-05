8923047f1cd0ff60943a983069039285fda07dc2
yangwenxin45 <yangwenxin45@gmail.com>
2020-02-05 10:35:41 +0800

    初始化项目

---
+++ src/test/java/net/yangwenxin/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yangwenxin.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
